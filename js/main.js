32eff0d19e80a0ada838b2d6057d2caed5c153c0
Nikita <miracleboy3228@mail.ru>
2020-10-31 14:45:04 +0300

    nerds complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+const contactsButton = document.querySelector('.write-us-button');
+const modalWriteUs = document.querySelector('.modal-write-us');
+const closeWriteUs = document.querySelector('.close-write-us');
+const form = modalWriteUs.querySelector('form');
+const nameField = modalWriteUs.querySelector('[name=name]');
+const emailField = modalWriteUs.querySelector('[name=email]');
+const messageField = modalWriteUs.querySelector('[name=text]');
+
+contactsButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalWriteUs.classList.add('modal-show');
+  nameField.focus();
+});
+
+closeWriteUs.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalWriteUs.classList.remove('modal-show');
+  nameField.classList.remove('write-us-input-invalid');
+  emailField.classList.remove('write-us-input-invalid');
+  messageField.classList.remove('write-us-input-invalid');
+});
+
+form.addEventListener('submit', function (evt) {
+  if (!nameField.value || !nameField.validity.valid) {
+    nameField.classList.add('write-us-input-invalid');
+    evt.preventDefault();
+  } else {
+    nameField.classList.remove('write-us-input-invalid');
+  }
+  if (!emailField.value || !emailField.validity.valid) {
+    emailField.classList.add('write-us-input-invalid');
+    evt.preventDefault();
+  } else {
+    emailField.classList.remove('write-us-input-invalid');
+  }
+  if (!messageField.value) {
+    messageField.classList.add('write-us-input-invalid');
+    evt.preventDefault();
+  } else {
+    messageField.classList.remove('write-us-input-invalid');
+  }
+});
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
